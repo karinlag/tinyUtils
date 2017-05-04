c93cbe4b88791223da3918b2dbb1e8d5126db210
Karin Lagesen <karin.lagesen@gmail.com>
2017-05-04 15:44:40 +0200

    Added script for extracting seqs by name to stdout

---
+++ extractNamedSeq.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from Bio import SeqIO
+
+name = sys.argv[1]
+seqfile = sys.argv[2]
+
+dirname = os.path.dirname(seqfile)
+
+
+for record in SeqIO.parse(seqfile, "fasta"):
+    if name in record.description:
+        record.id = dirname + "_" + record.id
+        fo = open('/dev/stdout', "w")
+        SeqIO.write(record, fo, "fasta")
+
+
+        
+
